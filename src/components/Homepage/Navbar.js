2de48ef9de695d1d0742f033ce6e8e019c2e2799
Devgr72 <devgrover72@gmail.com>
2024-10-11 03:05:16 +0530

    nav change

--- src/components/Homepage/Navbar.js
+++ src/components/Homepage/Navbar.js
@@ -1,51 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../Assets/logo-video.mp4'; 
 import '../../App.css';
 
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { logout } from './Userslice'; // Import the logout action
+import { logout } from './Userslice'; 
 
 function Navbar() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
   const navigate = useNavigate();
-  const dispatch = useDispatch();  
+  const dispatch = useDispatch();
   const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
   const userName = useSelector((state) => state.user.name);
+
   const handleLogout = () => {
     dispatch(logout()); 
     navigate('/'); 
   };
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
   return (
     <>
-      {/* navbar code */}
       <nav className="main-nav">
         {/* Logo */}
-        <div className='logo'>
-          <div className='login-video'>
-            <video width="400px" height="120px" autoPlay loop muted>
-              <source src={logo} type="video/mp4" />
-            </video>
-            <h1 className='img-h1'>Explora</h1>
-          </div>
+        <div className='login-video'>
+          <video width="100px" height="50px" autoPlay loop muted>
+            <source src={logo} type="video/mp4" />
+          </video>
+          <h1 className='img-h1'>Explora</h1>
         </div>
+
+        {/* Hamburger Menu */}
+        <div className="hamburger" onClick={toggleSidebar}>
+          &#9776; {/* Hamburger icon */}
+        </div>
+      </nav>
+
+      {/* Sidebar */}
+      <div className={`sidebar ${sidebarOpen ? 'open' : ''}`}>
+        {/* Close Button */}
+        <span className="sidebar-close" onClick={toggleSidebar}>&times;</span>
+        
         {/* Main menu */}
-        <div className='main-menu'>
-          <ul>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            <li>
-              <Link to='/aboutus'>About</Link>
-            </li>
-            <li>
-              <Link to='/services'>Services</Link>
-            </li>
-            <li>
-              <a href='#'>Booking</a>
-            </li>
-          </ul>
-        </div>
-        <div className='login-signup'>
+        <ul>
+          <li>
+            <Link to='/' onClick={toggleSidebar}>Home</Link>
+          </li>
+          <li>
+            <Link to='/aboutus' onClick={toggleSidebar}>About</Link>
+          </li>
+          <li>
+            <Link to='/services' onClick={toggleSidebar}>Services</Link>
+          </li>
+          <li>
+            <a href='#' onClick={toggleSidebar}>Booking</a>
+          </li>
+        </ul>
+
+        {/* Login/Signup buttons */}
+        <div className='sidebar-login-signup'>
           {isLoggedIn ? (
             <>
               <span>Welcome, {userName}!</span>
@@ -53,19 +69,16 @@ function Navbar() {
             </>):
             (
             <>
-              <Link to='/login'>
-                <button className="btn btn-primary" type="button">Signup</button>
+              <Link to='/login' onClick={toggleSidebar}>
+                <button className="btn btn-primary">Signup</button>
               </Link>
-              <Link to='/loginpage'>
-                <button className="btn btn-primary" type="button">Login</button>
+              <Link to='/loginpage' onClick={toggleSidebar}>
+                <button className="btn btn-primary">Login</button>
               </Link>
             </>
           )}
         </div>
-      </nav>
-      <section className='banner'>
-        {/* <img src={banner} ></img> */}
-      </section>
+      </div>
     </>
   );
 }
