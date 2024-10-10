d77233b3e27502d987ced3c9904a26e64f01d001
Devgr72 <devgrover72@gmail.com>
2024-10-11 02:23:46 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+// import Banner from './components/Homepage/Banner';
+import Home from './components/Homepage/Home';
+import Login from '../src/components/Login-signup/Login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Aboutus from './components/about-us/Aboutus';
+import Service from './components/services/Service';
+import LoginPage from './components/Login-signup/LoginPage';
+import ForgetPass from './components/Login-signup/ForgetPass';
+import Bookservice from './components/service-booking/Bookservice';
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path ='/aboutus' element={<Aboutus/>}/>
+    <Route path ='/services' element={<Service/>}/>
+    <Route path='/loginpage' element={<LoginPage/>}/>
+    <Route path="/bookservice" element={<Bookservice/>} />
+    <Route path='/login' element={<Login/>}/>
+    <Route path='/forget' element={<ForgetPass/>}/>
+     </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
